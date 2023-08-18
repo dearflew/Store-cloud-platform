b28830b1939952e30aa14948f97cf7a39eb9ed25
chengyunfei <847059646@qq.com>
2023-08-18 09:30:26 +0800

    初始化前后端

---
+++ store-cloud-platform-backend/start-center/src/main/java/com/linly/StartServiceApplication.java
@@ -0,0 +1,17 @@
+package com.linly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @PACKAGE_NAME: com.linly
+ * @NAME: StartServiceApplication
+ * @USER: chengyunfei
+ * @DATE: 2023/8/17 9:14
+ */
+@SpringBootApplication
+public class StartServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StartServiceApplication.class, args);
+    }
+}
